025390f279e1881e0f493b312290bacabb5c362f
lubb <2820843645@qq.com>
2024-03-21 15:31:05 +0800

    vr

--- src/main/java/com/xpu/sceneryview/controller/VRController.java
+++ src/main/java/com/xpu/sceneryview/controller/VRController.java
@@ -3,15 +3,13 @@ package com.xpu.sceneryview.controller;
 import com.xpu.sceneryview.entity.Result;
 import com.xpu.sceneryview.entity.VRInfo;
 import com.xpu.sceneryview.service.impl.VRServiceImpl;
-import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 @RestController
 @Slf4j
